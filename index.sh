3b61096b430f21e1940373f43ce66d1940be5b94
afeiship <1290657123@qq.com>
2017-03-01 11:02:01 +0800

    update

--- index.sh
+++ index.sh
@@ -5,6 +5,6 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 ## basic paths:
 DACANG_BUILD_PATH='/root/git-oschina/dacang-build';
 
-alias dc-qunpai = "$DACANG_BUILD_PATH/src/qunpai/qunpai_to_test.rb";
+alias dc-qunpai="$DACANG_BUILD_PATH/src/qunpai/qunpai_to_test.rb"
 
 unset ROOT_PATH;
